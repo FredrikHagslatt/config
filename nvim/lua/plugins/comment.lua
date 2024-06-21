564bf6f8a8b45acc3250565dff25b0ad40b7abb2
Fredrik Hagslätt <fredrik.hagslatt@gmail.com>
2024-06-21 23:08:50 +0200

    cpp comment // instead of /* .. */

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -9,6 +9,14 @@ local setup = {
   line_mapping = "<leader>cc",
   operator_mapping = "<leader>c",
   comment_chunk_text_object = "<leader>cb",
+  hook = function()
+    local ft = vim.api.nvim_buf_get_option(0, "filetype")
+    if ft == "cpp" or ft == "c" then
+      vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+    elseif ft == "lua" then
+      vim.api.nvim_buf_set_option(0, "commentstring", "-- %s")
+    end
+  end
 }
 
 plugin.setup(setup)
